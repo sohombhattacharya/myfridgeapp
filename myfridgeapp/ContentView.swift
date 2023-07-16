705736970957dc281eb0ffc8f1f316d731dca67c
sohombhattacharya <sohom95@gmail.com>
2023-07-16 16:31:54 -0400

    Initial Commit

---
+++ myfridgeapp/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  myfridgeapp
+//
+//  Created by Sohom Bhattacharya on 7/16/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ myfridgeapp/myfridgeappApp.swift
@@ -0,0 +1,17 @@
+//
+//  myfridgeappApp.swift
+//  myfridgeapp
+//
+//  Created by Sohom Bhattacharya on 7/16/23.
+//
+
+import SwiftUI
+
+@main
+struct myfridgeappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
